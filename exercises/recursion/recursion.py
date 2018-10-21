f3a5083bff875510fdcd7aa2838754f09025af2c
Swopnil N. Shrestha <swopnilnep@gmail.com>
2018-10-21 15:11:16 -0500

    Finish recursion

--- exercises/recursion/recursion.py
+++ exercises/recursion/recursion.py
@@ -28,23 +28,35 @@ def diamond_ite(levels: int) -> None:
         levels += adder
         if levels == 1: adder *= -1
 
-def hourglass_rec(levels: int, initial=levels) -> None:
-    initial = levels
-    if levels == initial + 1: return None
-    print(levels)
-    hourglass_rec(levels - 1, initial)
-    
-    
+def hourglass_rec(levels: int, space=0) -> None:
+  str = (space * " ") + (2 * levels - 1) * "*"
 
-def diamond_rec(levels: int) -> None:
-    raise NotImplementedError
+  if levels == 1: 
+    print(str)
+    return
+
+  print(str)  # print down 5, 4, 3, 2, 1
+  hourglass_rec(levels - 1, space + 1)  # recursion point
+  print(str)
+
+def diamond_rec(levels: int, space=0) -> None:
+      
+  str = (levels * " ") + (2 * space + 1) * "*"
+  
+  if levels == 1: 
+    print(str)
+    return
+
+  print(str)
+  diamond_rec(levels - 1, space + 1)  # recursion point
+  print(str)
 
 def main():
     '''Main function'''
-    # hourglass_ite(5)
-    hourglass_rec(5)
-    # diamond_ite(5)
-    # diamond_rec(5)
+    hourglass_ite(10)
+    hourglass_rec(10)
+    diamond_ite(10)
+    diamond_rec(10)
 
 
 if __name__ == '__main__':
